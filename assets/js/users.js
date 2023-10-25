169e53951ee7d708072b5062138d53c331d1c587
zerodevelpr_git <iman6978ranjbar@gmail.com>
2023-10-26 03:23:20 +0330

    add & styling "add user modal"

--- assets/js/users.js
+++ assets/js/users.js
@@ -1,4 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
   var elems = document.querySelectorAll(".collapsible");
   var instances = M.Collapsible.init(elems, {});
+
+  const addUserButton = document.getElementById("add-user-button");
+  const addUserModalBack = document.getElementById("add-user-modal-back");
+  const addUserModal = document.getElementById("add-user-modal");
+  
+  addUserButton.addEventListener("click", () => {
+    addUserModalBack.classList.remove("hidden");
+    addUserModal.classList.add("show");
+  });
+
+  addUserModalBack.addEventListener("click", (e) => {
+    e.target.classList.add("hidden");
+    addUserModal.classList.remove("show");
+  });
 });
