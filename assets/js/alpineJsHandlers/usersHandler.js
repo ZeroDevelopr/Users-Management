b6ea18bd699b9806cae1df332ab9d1a8a96f5ad2
zerodevelpr_git <iman6978ranjbar@gmail.com>
2024-01-15 01:15:13 +0330

    add preloader of user list

--- assets/js/alpineJsHandlers/usersHandler.js
+++ assets/js/alpineJsHandlers/usersHandler.js
@@ -1,9 +1,11 @@
 document.addEventListener("alpine:init", () => {
   Alpine.data("usersData", () => ({
     users: [],
+    isLoading: true,
     initUsers() {
       axios.get("https://jsonplaceholder.typicode.com/users").then((res) => {
         this.users = res.data;
+        this.isLoading = false;
       });
     },
   }));
